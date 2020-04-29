245fc7ffc9c96c29ba756c91cbb8edf7257c5a6e
Inho Choi <inho.choi@menlosecurity.com>
2020-04-29 00:18:57 -0700

    Add TF and run jupyter

---
+++ tf/docker-run-cli.sh
@@ -0,0 +1,1 @@
+docker run -it --rm -p 8888:8888 --name tf2-py3-jupyter-testrun tf2-py3-jupyter:latest /bin/bash
--- tf/docker-run.sh
+++
@@ -1,1 +0,0 @@
-docker run -it --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes --name tf2-py3-jupyter-testrun tf2-py3-jupyter:latest
